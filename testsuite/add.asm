68712db511312693ec678c17b03658f23e45e37e
Jakub Szewczyk <kubasz51@gmail.com>
2012-10-13 19:36:59 +0200

    Even more changes to the testsuite

--- testsuite/add.asm
+++ testsuite/add.asm
@@ -1,6 +1,12 @@
-xor al,al
+kmain:
+	xor al,al
+	loop1:
+		daa
+		inc al
+		jnz loop1
+	nop
+jmp near kmain
+align 8176, nop
+jmp near kmain
+align 8192, nop
 
-loop:
-	daa
-	inc al
-	jnz loop
---
+++ testsuite/compile.sh
@@ -0,0 +1,2 @@
+echo Compiling $1.asm
+yasm -f bin -o $1.rom $1.asm
