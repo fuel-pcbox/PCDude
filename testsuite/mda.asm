67519ccf4e00e2f17a6590ae48a1661a65ecd79c
Jakub Szewczyk <kubasz51@gmail.com>
2012-10-14 20:29:44 +0200

    More MDA testing code

--- testsuite/mda.asm
+++ testsuite/mda.asm
@@ -8,6 +8,27 @@ kmain:
 	mov byte [bx], 'A'
 	inc bx
 	mov byte [bx], 0x09
+	
+	mov dx,0x3B8
+	mov al,0x1B
+	out dx,al
+	mov ax,0xB000
+	mov ds,ax
+	mov bx,2
+	mov byte [bx], 'B'
+	inc bx
+	mov byte [bx], 0x0A
+	
+	mov dx,0x3B8
+	mov al,0x1B
+	out dx,al
+	mov ax,0xB000
+	mov ds,ax
+	mov bx,4
+	mov byte [bx], 'C'
+	inc bx
+	mov byte [bx], 0x70
+	
 	nop
 jmp near kmain
 times 8176 - ($-$$) db 90
